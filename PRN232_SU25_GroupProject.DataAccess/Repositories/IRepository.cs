59dd08ea58f3cf113fd788eb8b9f42d4cbce0291
DrStrong177013 <149897720+DrStrong177013@users.noreply.github.com>
2025-06-19 12:06:51 +0700

    Implement UnitOFWork

---
+++ PRN232_SU25_GroupProject.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_GroupProject.DataAccess.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(object id);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ PRN232_SU25_GroupProject.DataAccess/Repositories/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using PRN232_SU25_GroupProject.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_GroupProject.DataAccess.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Student> Students { get; }
+        IRepository<Parent> Parents { get; }
+        IRepository<MedicalProfile> MedicalProfiles { get; }
+        IRepository<Medication> Medications { get; }
+        IRepository<MedicalIncident> MedicalIncidents { get; }
+        IRepository<VaccinationCampaign> VaccinationCampaigns { get; }
+        // ... thêm các repository cần dùng
+
+        Task<int> CompleteAsync();
+    }
+}
